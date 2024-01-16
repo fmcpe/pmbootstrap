1bb228fcfd057eb6a493a1275ce374821fb2a652
Oliver Smith <ollieparanoid@postmarketos.org>
2024-01-16 22:33:06 +0000

    README: point to new source location

--- pmb/export/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2023 Oliver Smith
-# SPDX-License-Identifier: GPL-3.0-or-later
-from pmb.export.frontend import frontend
-from pmb.export.odin import odin
-from pmb.export.symlinks import symlinks
